875ae6c6f321fa58e67153218a2180c64e53bfb6
PierfrancescoConti <pconti97@libero.it>
2020-10-04 18:57:59 +0200

    run setup.sh to download tools and update VulnDB

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+cd tools
+
+echo -e '\n\n\033[37;1m\033[44;1m             Starting installation             \033[0m\n'
+
+echo -e '\n\033[34;1m Cleaning... \033[0m\n'
+rm -rf ./*
+echo -e "\033[32;1m Done, let's start!\033[0m\n"
+
+
+echo -e '\n\n\033[34;1m Obtaining tools...\033[0m\n'
+
+git clone https://github.com/scipag/vulscan.git
+git clone https://github.com/PierfrancescoConti/LiteRespH.git
+git clone https://github.com/maurosoria/dirsearch.git
+git clone https://github.com/drwetter/testssl.sh.git
+
+echo -e '\n\033[32;1m Cloned repositories - tools are ready!\033[0m\n'
+
+
+
+
+echo -e '\n\n\033[34;1m Updating VulnDB databases...\033[0m\n'
+
+cd ..
+cp updateFiles.sh tools/vulscan/utilities/updater/updateFiles.sh
+cd tools/vulscan/utilities/updater/
+chmod +x updateFiles.sh
+./updateFiles.sh
+
+echo -e '\n\033[32;1m All is done!\033[0m\n'
+
+echo -e '\n\n\033[37;1m\033[44;1m                      End                      \033[0m\n'
+
